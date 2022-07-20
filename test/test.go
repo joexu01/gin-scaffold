dc7e64ae9dd65a3c91ba24e06cbb8a97aebb1fd3
Joseph Xu <joexu01@yahoo.com>
2022-07-20 18:44:38 +0800

    daily commit on 20, July

---
+++ test/test.go
@@ -0,0 +1,53 @@
+package main
+
+func main() {
+	//connStr := "root:atk_2018@tcp(127.0.0.1:3306)/gin_test?charset=utf8mb4&parseTime=True&loc=Local"
+	//db, err := gorm.Open(mysql.Open(connStr), &gorm.Config{})
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//userInsert := dto.NewUserInput{
+	//	Username:    "joexu01",
+	//	RawPassword: "12345678",
+	//	Email:       "joexu01@yahoo.com",
+	//}
+	//
+	//pwdHash, err := public.GeneratePwdHash([]byte(userInsert.RawPassword))
+	//if err != nil {
+	//	panic(err)
+	//}
+	//user := dao.User{
+	//	Id:        0,
+	//	Username:  userInsert.Username,
+	//	Password:  pwdHash,
+	//	Email:     userInsert.Email,
+	//	CreatedAt: time.Now(),
+	//	UpdatedAt: time.Now(),
+	//	IsDelete:  0,
+	//}
+	//
+	//result := db.Create(&user)
+	//
+	//fmt.Println(user.Id)
+	//
+	//fmt.Printf("%+v", result)
+	//
+	//user1 := dao.User{
+	//	Id:        0,
+	//	Username:  "joexu01",
+	//	Password:  "",
+	//	Email:     "",
+	//	CreatedAt: time.Time{},
+	//	UpdatedAt: time.Time{},
+	//	IsDelete:  0,
+	//}
+	//
+	//result = db.First(&user1)
+	//
+	//fmt.Println(user1.Email)
+	//fmt.Printf("%+v", result)
+
+	s := "hello"
+	println(len(s))
+}
